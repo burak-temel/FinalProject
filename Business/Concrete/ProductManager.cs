aef495e97efd62683098a474f170c5bc01268a44
Burak <burak.temel.07@icloud.com>
2021-10-10 12:30:25 +0300

    Resolve Failure of Recent Project

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            // Koşullar burada yazılacak. Örnek yetkisi var mı?
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetAllByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice>= min && p.UnitPrice<=max);
+
+        }
+
+        public List<ProductDetailDto> GetProductDetailDtos()
+        {
+            return _productDal.GetProductDetails();
+        }
+    }
+}
---
+++ Core1/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    // Veri tabanı tablosu
+    public interface IEntity
+    {
+
+    }
+}
