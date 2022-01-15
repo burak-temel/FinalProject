d0b169f1e54eae781799bc869861ed42b500b48d
Burak <burak.temel.07@icloud.com>
2022-01-15 12:19:11 +0300

    Added Authorazation Token structure

--- Business/Constant/Messages.cs
+++ Business/Constant/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constant
@@ -11,6 +13,14 @@ namespace Business.Constant
         public static string ProductNameInvalid = "Product name invalid";
         public static string MaintenanceTime = "System currently maintenanced";
         public static string ProductListed = "System currently maintenanced";
-        internal static string CategoryOutofLimit= "Category Out Of limit";
+        public static string CategoryOutofLimit = "Category Out Of limit";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserAlreadyExists = "Kullanıcı zaten bulunmakta";
+        public static string UserRegistered = "Kullanıcı kaydedildi";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Kullanıcı adı ya da parola yanlış";
+        public static string SuccessfulLogin = "Giriş yapıldı";
+        public static string AccessTokenCreated = "Token Oluşturuldu";
+
     }
 }
